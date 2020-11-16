f30f2e6c5c86d71877732d2a9751a4dc672b96f4
Ken Duck <kduck@sonatype.com>
2020-11-16 02:02:42 -0400

    Added some test files from Jeffry. These need checking because they are failing (likely my fault).

---
+++ bom/bom_test.go
@@ -0,0 +1,158 @@
+// Copyright 2020 Sonatype Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package bom
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/package-url/packageurl-go"
+	"github.com/sonatype-nexus-community/cheque/types"
+)
+
+const DPKGRESPONSE = "Package: debtest\nStatus: install ok installed\nVersion: 1.2.3"
+
+type FakeLDDCommand struct {
+}
+
+func (f FakeLDDCommand) ExecCommand(args ...string) ([]byte, error) {
+	byteResponse := []byte("linux-vdso.so.1 => (0x00007fff07fe0000)\nlibc.so.6 => /lib64/libc.so.6 (0x00007f4b3e3c8000)\n/lib64/ld-linux-x86-64.so.2 (0x00007f4b3e9ab000)")
+	return byteResponse, nil
+}
+
+func (f FakeLDDCommand) IsValid() bool {
+	return true
+}
+
+type FakeRPMCommand struct {
+}
+
+func (r FakeRPMCommand) ExecCommand(args ...string) ([]byte, error) {
+	if strings.Contains(args[2], "rpmtest") {
+		if args[1] == "--whatprovides" {
+			return []byte("rpmtest-1.2.3-260.175.amzn1.x86_64"), nil
+		}
+		return []byte("Name        : rpmtest\nVersion     : 1.2.3"), nil
+	}
+	return []byte(fmt.Sprintf("file %s is not owned by any package", args[2])), fmt.Errorf("file %s is not owned by any package", args[2])
+}
+
+func (r FakeRPMCommand) IsValid() bool {
+	return true
+}
+
+type FakeDEBCommand struct {
+}
+
+func (d FakeDEBCommand) ExecCommand(args ...string) ([]byte, error) {
+	if strings.Contains(args[1], "debtest") {
+		if args[0] == "-S" {
+			return []byte("debtest:amd64: /lib64/ld-linux-x86-64.so.2"), nil
+		}
+		return []byte(DPKGRESPONSE), nil
+	}
+	return []byte(fmt.Sprintf("file %s is not owned by any package", args[1])), fmt.Errorf("file %s is not owned by any package", args[1])
+}
+
+func (d FakeDEBCommand) IsValid() bool {
+	return true
+}
+
+func TestUnixCreateBom(t *testing.T) {
+	SetupTestUnixFileSystem(UBUNTU)
+	LDDCommand = FakeLDDCommand{}
+	RPMExtCmd = FakeRPMCommand{}
+	DEBExtCmd = FakeDEBCommand{}
+	deps, err := CreateBom([]string{"/usrdefined/path"},
+		[]string{"bob", "ken", "pkgtest", "rpmtest", "debtest"},
+		[]string{"/lib/libpng.so", "/lib/libtiff.a", "/lib/libsnuh.so.1.2.3", "/lib/libbuh.4.5.6.so"})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Print(deps)
+	// Path based results
+	assertResultContains(t, deps, "pkg:cpp/libbob@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/bob@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/libsnuh@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/snuh@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/libbuh@4.5.6")
+	assertResultContains(t, deps, "pkg:cpp/buh@4.5.6")
+
+	// pkgconfig based results
+	assertResultContains(t, deps, "pkg:cpp/libken@2.3.4")
+	assertResultContains(t, deps, "pkg:cpp/ken@2.3.4")
+	assertResultContains(t, deps, "pkg:cpp/pkgtest@3.4.5")
+	assertResultContains(t, deps, "pkg:cpp/pkgtest@3.4.5")
+
+	// OS based results
+	assertResultContains(t, deps, "pkg:rpm/rpmtest@1.2.3")
+	assertResultContains(t, deps, "pkg:deb/ubuntu/debtest@1.2.3")
+
+	// Should not get more than 12 results
+	if len(deps.Projects) != 12 {
+		t.Error(fmt.Sprintf("Expecting twelve (12) package in BOM, found %v", len(deps.Projects)))
+	}
+}
+
+func TestOsxCreateBom(t *testing.T) {
+	SetupTestOSXFileSystem(OSX)
+	LDDCommand = FakeLDDCommand{}
+	deps, err := CreateBom([]string{"/usrdefined/path"},
+		[]string{"bob", "ken", "pkgtest"},
+		[]string{"/lib/libpng.dylib", "/lib/libsnuh.1.2.3.dylib", "/lib/libbuh.4.5.6.dylib"})
+
+	if err != nil {
+		t.Error(err)
+	}
+	// Path based results
+	assertResultContains(t, deps, "pkg:cpp/libbob@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/bob@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/libsnuh@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/snuh@1.2.3")
+	assertResultContains(t, deps, "pkg:cpp/libbuh@4.5.6")
+	assertResultContains(t, deps, "pkg:cpp/buh@4.5.6")
+
+	// pkgconfig based results
+	assertResultContains(t, deps, "pkg:cpp/libken@2.3.4")
+	assertResultContains(t, deps, "pkg:cpp/ken@2.3.4")
+	assertResultContains(t, deps, "pkg:cpp/pkgtest@3.4.5")
+	assertResultContains(t, deps, "pkg:cpp/libpkgtest@3.4.5")
+
+	// Should not get more than 10 results
+	if len(deps.Projects) != 10 {
+		t.Error(fmt.Sprintf("Expecting ten (10) package in BOM, found %v", len(deps.Projects)))
+	}
+}
+
+func assertResultContains(t *testing.T, deps types.ProjectList, pstring string) {
+	purl, err := packageurl.FromString(pstring)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for _, p := range deps.Projects {
+		if p.Type == purl.Type &&
+			p.Namespace == purl.Namespace &&
+			p.Name == purl.Name &&
+			p.Version == purl.Version {
+			return
+		}
+	}
+
+	t.Error("Missing expected PURL: " + purl.String())
+}
---
+++ bom/libs_test.go
@@ -0,0 +1,196 @@
+// Copyright 2020 Sonatype Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package bom
+
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+)
+
+const UBUNTU = "ubuntu"
+
+const OSX = "darwin"
+
+// UNIX TESTS
+func TestUnixGetLibraryPathBasic(t *testing.T) {
+	SetupTestUnixFileSystem(UBUNTU)
+	_, err := GetLibraryPath(nil, "libpng.so")
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func TestUnixGetLibraryPath(t *testing.T) {
+	SetupTestUnixFileSystem(UBUNTU)
+	path, err := GetLibraryPath([]string{"/lib"}, "libpng.so")
+	if err != nil {
+		t.Error(err)
+	}
+	if path == "" {
+		t.Errorf("Error, path is %s", path)
+	}
+}
+
+func TestUnixGetLibraryPathWithoutLibOrExtension(t *testing.T) {
+	SetupTestUnixFileSystem(UBUNTU)
+	path, err := GetLibraryPath([]string{"/lib"}, "png")
+	if err != nil {
+		t.Error(err)
+	}
+	if path == "" {
+		t.Errorf("Error, path is %s", path)
+	}
+}
+
+func TestUnixGetLibraryName(t *testing.T) {
+	Goose = UBUNTU
+	name, err := GetLibraryName("libpng.2.0.5.so")
+	if err != nil {
+		t.Error(err)
+	}
+	if name != "libpng" {
+		t.Errorf("Error, name is %s", name)
+	}
+}
+
+func TestUnixGetLibraryNameWithVersionLast(t *testing.T) {
+	Goose = UBUNTU
+	name, err := GetLibraryName("libpng.so.2.0.5")
+	if err != nil {
+		t.Error(err)
+	}
+	if name != "libpng" {
+		t.Errorf("Error, name is %s", name)
+	}
+}
+
+func TestUnixGetLibraryVersion(t *testing.T) {
+	Goose = UBUNTU
+	version, err := GetLibraryVersion("libpng.2.0.5.so")
+	if err != nil {
+		t.Error(err)
+	}
+	if version != "2.0.5" {
+		t.Errorf("Error, version is %s", version)
+	}
+}
+
+func TestUnixGetLibraryVersionWithVersionLast(t *testing.T) {
+	Goose = UBUNTU
+	version, err := GetLibraryVersion("libpng.so.2.0.5")
+	if err != nil {
+		t.Error(err)
+	}
+	if version != "2.0.5" {
+		t.Errorf("Error, version is %s", version)
+	}
+}
+
+// DARWIN TESTS
+
+func TestDarwinGetLibraryPathBasic(t *testing.T) {
+	SetupTestOSXFileSystem(OSX)
+	_, err := GetLibraryPath(nil, "libpng.dylib")
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func TestDarwinGetLibraryPath(t *testing.T) {
+	SetupTestOSXFileSystem(OSX)
+	path, err := GetLibraryPath([]string{"/lib"}, "libpng.dylib")
+	if err != nil {
+		t.Error(err)
+	}
+	if path == "" {
+		t.Errorf("Error, path is %s", path)
+	}
+}
+
+func TestDarwinGetLibraryPathWithoutLibOrExtension(t *testing.T) {
+	SetupTestOSXFileSystem(OSX)
+	path, err := GetLibraryPath([]string{"/lib"}, "png")
+	if err != nil {
+		t.Error(err)
+	}
+	if path == "" {
+		t.Errorf("Error, path is %s", path)
+	}
+}
+
+func TestDarwinGetLibraryName(t *testing.T) {
+	Goose = OSX
+	name, err := GetLibraryName("libpng.2.0.5.dylib")
+	if err != nil {
+		t.Error(err)
+	}
+	if name != "libpng" {
+		t.Errorf("Error, name is %s", name)
+	}
+}
+
+func TestDarwinGetLibraryVersion(t *testing.T) {
+	Goose = OSX
+	version, err := GetLibraryVersion("libpng.2.0.5.dylib")
+	if err != nil {
+		t.Error(err)
+	}
+	if version != "2.0.5" {
+		t.Errorf("Error, version is %s", version)
+	}
+}
+
+// FILESYSTEM HELPERS
+
+func SetupTestUnixFileSystem(operating string) {
+	Goose = operating
+	AppFs = afero.NewMemMapFs()
+
+	AppFs.MkdirAll("/lib/pkgconfig", 0755)
+	afero.WriteFile(AppFs, "/lib/libpng.so", []byte("file b"), 0644)
+	afero.WriteFile(AppFs, "/lib/libtiff.a", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/lib/libsnuh.so.1.2.3", []byte("file d"), 0644)
+	afero.WriteFile(AppFs, "/lib/libbuh.4.5.6.so", []byte("file e"), 0644)
+
+	AppFs.MkdirAll("/usrdefined/path/pkgconfig", 0755)
+	afero.WriteFile(AppFs, "/usrdefined/path/libbob.so.1.2.3", []byte("file b"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libken.so", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/pkgconfig/libken.pc", []byte("Name: ken\nVersion: 2.3.4"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libpkgtest.so", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/pkgconfig/libpkgtest.pc", []byte("Name: pkgtest\nVersion: 3.4.5"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/librpmtest.so", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libdebtest.so", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libstatic.a", []byte("file c"), 0644)
+}
+
+func SetupTestOSXFileSystem(operating string) {
+	Goose = operating
+	AppFs = afero.NewMemMapFs()
+
+	AppFs.MkdirAll("/lib/pkgconfig", 0755)
+	afero.WriteFile(AppFs, "/lib/libpng.dylib", []byte("file b"), 0644)
+	afero.WriteFile(AppFs, "/lib/libsnuh.1.2.3.dylib", []byte("file d"), 0644)
+	afero.WriteFile(AppFs, "/lib/libbuh.4.5.6.dylib", []byte("file e"), 0644)
+
+	AppFs.MkdirAll("/usrdefined/path/pkgconfig", 0755)
+	afero.WriteFile(AppFs, "/usrdefined/path/libbob.1.2.3.dylib", []byte("file b"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libken.dylib", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/pkgconfig/libken.pc", []byte("Name: ken\nVersion: 2.3.4"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libpkgtest.dylib", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/pkgconfig/libpkgtest.pc", []byte("Name: pkgtest\nVersion: 3.4.5"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/librpmtest.dylib", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libdebtest.dylib", []byte("file c"), 0644)
+	afero.WriteFile(AppFs, "/usrdefined/path/libstatic.a", []byte("file c"), 0644)
+}
